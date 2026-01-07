66d95312ee455fac4e100b0b6070440a36e041be
Code Nova <netdot1234@gmail.com>
2026-01-07 10:56:40 +0100

    5000

--- main.js
+++ main.js
@@ -477,7 +477,7 @@ function startImageTransitions() {
           productImage.src = product.images[randomIndex];
           productImage.style.opacity = 1;
         }, 500);
-      }, 3000);
+      }, 5000);
     }
   });
 }
